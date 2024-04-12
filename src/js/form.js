91021473cc54677f03f9518abefcb2da4302daa5
luciadelafuente <luciadelaf1991@gmail.com>
2024-04-12 12:51:14 +0200

    create object

--- src/js/form.js
+++ src/js/form.js
@@ -8,25 +8,37 @@ const emailPreview = document.querySelector('.js__preview_email');
 const linkedinPreview = document.querySelector('.js__preview_linkedn');
 const githubPreview = document.querySelector('.js__preview_github');
 
+const cardData = {
+  palette: "",
+  name: "",
+  job: "",
+  phone: "",
+  email: "",
+  linkedin: "",
+  github: "",
+  photo: "",
+};
+
 form.addEventListener('input', (event)=>{
     const input = event.target.id;
     const valueInput = event.target.value;
-    console.log(input);
+    cardData[input] = valueInput;
+    console.log(cardData);
 
-    if(input === 'firstName'){
+    if(input === 'name'){
         namePreview.innerHTML = valueInput;
     } else if(input === 'job'){
         jobPreview.innerHTML = valueInput;
-    } else if(input === 'emailAddress'){
+    } else if(input === 'email'){
         emailPreview.href = valueInput;
         console.log(input);
-    } else if(input === 'telNumber'){
+    } else if(input === 'phone'){
         phonePreview.href = valueInput;
         console.log(input);
-    } else if(input === 'socialMedia'){
+    } else if(input === 'linkedin'){
         linkedinPreview.href = valueInput;
         console.log(input);
-    } else if(input === 'git'){
+    } else if(input === 'github'){
         githubPreview.href = valueInput;
     }
 
